289fb98246ff627e3e8c6dbb2c044014248c843d
mssigler <mssigler@gmail.com>
2019-01-29 00:59:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,17 @@
 # code your #valid_move? method here
-
-
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+#  selection does not fall in the range of 0 through 8, their input is invalid.    Look up the #between? method for starters.
+
+
+def valid_move?
+
+
+  def position_taken?(array, index)
+    if ( array[index] == " " ||  array[index] ==""  || array[index] == nil )
+      return FALSE
+    else
+      return TRUE
+    end
+  end
+  
+end
